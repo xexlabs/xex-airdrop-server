7cf09f5071324d1ff7cf65da73f67be4a9a9e8a5
regen <120606503+xexcrypto@users.noreply.github.com>
2023-03-14 13:01:15 -0300

    cors fix, added test wallet

--- proof/src/index.js
+++ proof/src/index.js
@@ -1,76 +1,74 @@
 'use strict';
 const merkle = require("@openzeppelin/merkle-tree");
 import dataTestnet from './data/testnet.txt';
+
 const networkInfo = {
-	"97": "bsc-testnet",
-	"43113": "avax-testnet",
-	"5": "eth-testnet",
-	"80001": "polygon-testnet",
-	"4002": "ftm-testnet",
+    "97": "bsc-testnet",
+    "43113": "avax-testnet",
+    "5": "eth-testnet",
+    "80001": "polygon-testnet",
+    "4002": "ftm-testnet",
 }
 
-const corsHeaders = {
-	'Access-Control-Allow-Origin': '*',
-	'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
-	'Access-Control-Max-Age': '86400',
-};
-
 function checkProof(address) {
-	if (!address) {
-		return {error: `invalid address.`};
-	}
-	const fileData = dataTestnet.trim().split('\n');
-	let airdrop_data = [];
-	for (let i in fileData) {
-		const lineData = fileData[i].trim();
-		if (!lineData) {
-			console.log(`Invalid line at ${i}`);
-			continue;
-		}
-		const userData = lineData.split(',');
-		airdrop_data.push([userData[0].toLowerCase(), userData[1]]);
-	}
-	const tree = merkle.StandardMerkleTree.of(airdrop_data, ["address", "uint256"]);
-	const MerkleTreeData = merkle.StandardMerkleTree.load(tree.dump());
+    if (!address) {
+        return {error: `invalid address.`};
+    }
+    const fileData = dataTestnet.trim().split('\n');
+    let airdrop_data = [];
+    for (let i in fileData) {
+        const lineData = fileData[i].trim();
+        if (!lineData) {
+            console.log(`Invalid line at ${i}`);
+            continue;
+        }
+        const userData = lineData.split(',');
+        airdrop_data.push([userData[0].toLowerCase(), userData[1]]);
+    }
+    const tree = merkle.StandardMerkleTree.of(airdrop_data, ["address", "uint256"]);
+    const MerkleTreeData = merkle.StandardMerkleTree.load(tree.dump());
 
-	const wallet = address.toLowerCase();
-	for (const [i, v] of MerkleTreeData.entries()) {
-		if (v[0] === wallet) {
-			const proof = MerkleTreeData.getProof(i);
-			const networkId = v[1];
-			const networkName = networkInfo[networkId];
-			console.log(`  proof found at ${i}, network: ${networkName} (${networkId}).`);
-			return {networkId: networkId, networkName: networkName, proof: proof};
-		}
-	}
-	return {error: `proof not found.`};
+    const wallet = address.toLowerCase();
+    for (const [i, v] of MerkleTreeData.entries()) {
+        if (v[0] === wallet) {
+            const proof = MerkleTreeData.getProof(i);
+            const networkId = v[1];
+            const networkName = networkInfo[networkId];
+            console.log(`  proof found at ${i}, network: ${networkName} (${networkId}).`);
+            return {networkId: networkId, networkName: networkName, proof: proof};
+        }
+    }
+    return {error: `proof not found.`};
 }
 
 export default {
-	async fetch(request, env, ctx) {
-		const url = new URL(request.url);
-		console.log('url', url);
-			if (request.method === 'OPTIONS') {
-				let respHeaders = {
-					...corsHeaders,
-					// Allow all future content Request headers to go back to browser
-					// such as Authorization (Bearer) or X-Client-Name-Version
-					'Access-Control-Allow-Headers': '*',
-				};
-				return new Response(null, {
-					headers: respHeaders,
-					Allow: 'GET, HEAD, POST, OPTIONS',
-				});
-			} else if (request.method === 'GET' || request.method === 'HEAD' || request.method === 'POST') {
-				const { searchParams } = new URL(request.url)
-				let wallet = searchParams.get('wallet')
-				const r = checkProof(wallet);
-				return new Response( JSON.stringify(r));
-			} else {
-					return new Response(null, {
-						status: 405,
-						statusText: 'Method Not Allowed',
-					})
-			}
-	},
+    async fetch(request, env, ctx) {
+        const url = new URL(request.url);
+        console.log(request.method, url);
+        if (request.method === 'OPTIONS') {
+            let respHeaders = {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
+                'Access-Control-Max-Age': '86400',
+                'Access-Control-Allow-Headers': '*',
+            };
+            return new Response(null, {
+                headers: respHeaders,
+                Allow: 'GET, HEAD, POST, OPTIONS',
+            });
+        } else if (request.method === 'GET' || request.method === 'HEAD' || request.method === 'POST') {
+            const {searchParams} = new URL(request.url)
+            const wallet = searchParams.get('wallet')
+            const proof = JSON.stringify(checkProof(wallet));
+            const response = new Response(proof);
+            response.headers.set('Access-Control-Allow-Origin', '*');
+            response.headers.append('Vary', 'Origin');
+            return response;
+        } else {
+            return new Response(null, {
+                status: 405,
+                statusText: 'Method Not Allowed',
+            })
+        }
+    },
 };
