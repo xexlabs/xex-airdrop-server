82db91db85b557362782a7ae096c606b88cea467
regen <regen@xexcrypto.com>
2023-03-21 21:44:47 -0300

    fixing ankr no events return

--- wl-scanner/index.js
+++ wl-scanner/index.js
@@ -5,11 +5,23 @@ const dotenv = require("dotenv");
 dotenv.config()
 
 const mainAddress = '0xa41A879bcFdd75983a987FD6b68fae37777e8b28';
-const factoryAddress = '0xA54108A0413f9992dFad7873966fF55d95EBcf7b';
-const abiMain = JSON.parse(fs.readFileSync("./abi/main-abi.js", "utf8"));
-const abiFactory = JSON.parse(fs.readFileSync("./abi/factory-abi.js", "utf8"));
+const abiMain = JSON.parse(fs.readFileSync("./abi/main-abi.json", "utf8"));
+const abiMinter = JSON.parse(fs.readFileSync("./abi/minter-abi.json", "utf8"));
 
-async function processChain(chainName, rpc, mainBlockStart, factoryBlockStart){
+
+
+async function processChain(maxUsers, chainName, rpc, mainBlockStart, factoryBlockStart){
+    console.log(`Scanning ${chainName} for ${maxUsers} first minters.`);
+    const web3 = new Web3(rpc);
+    const main = new web3.eth.Contract(abiMain, mainAddress);
+    const lastBlock = parseInt(await web3.eth.getBlockNumber());
+    try {
+        await scanBlockchain(web3, chainName, main, mainBlockStart, lastBlock, maxUsers);
+    }catch(e){
+        console.log(`Error running the chain ${chainName} scan: ${e.toString()}`);
+    }
+}
+async function main() {
     /*
     - ETH: First 500 Unique addresses to mint
     - MATIC: First 2500 Unique addresses to mint
@@ -17,58 +29,73 @@ async function processChain(chainName, rpc, mainBlockStart, factoryBlockStart){
     - AVAX: First 1500 Unique addresses to mint
     - FTM: First 2500 Unique addresses to mint
     * */
-    const web3 = new Web3(rpc);
-    const main = new web3.eth.Contract(abiMain, mainAddress);
-    const factory = new web3.eth.Contract(abiFactory, factoryAddress);
-    const lastBlock = parseInt(await web3.eth.getBlockNumber());
-    /*
-    try {
-        await scanBlockchain(blocks.BLOCK_START, blocks.BLOCK_END);
-    }catch(e){
-        console.log(`Error running the chain ${chainName} scan: ${e.toString()}`);
-    }
-    */
-}
-async function main() {
-    await processChain('bsc',`https://rpc.ankr.com/bsc/${process.env.ANKR}`, '25208900', '25208912');
-    await processChain('avalanche',`https://rpc.ankr.com/avalanche/${process.env.ANKR}`, '25570246', '25570259');
-    await processChain('polygon',`https://rpc.ankr.com/polygon/${process.env.ANKR}`, '38669109', '38669138');
-    await processChain('fantom',`https://rpc.ankr.com/fantom/${process.env.ANKR}`, '54668080', '54668094');
-    await processChain('ethereum',`https://mainnet.infura.io/v3/${process.env.INFURA}`, '16513351', '16513362');
-    await processChain('arbitrum',`https://arb1.arbitrum.io/rpc`, '65115100', '65115132');
+
+    // await processChain(1500, 'bsc',`https://rpc.ankr.com/bsc/${process.env.ANKR}`, '25208900', '25208912');
+    // await processChain(1500, 'avalanche',`https://rpc.ankr.com/avalanche/${process.env.ANKR}`, '25570246', '25570259');
+    // await processChain(2500, 'polygon',`https://rpc.ankr.com/polygon/${process.env.ANKR}`, '38669109', '38669138');
+    // await processChain(2500, 'fantom',`https://rpc.ankr.com/fantom/${process.env.ANKR}`, '54668080', '54668094');
+    await processChain(500, 'ethereum',`https://mainnet.infura.io/v3/${process.env.INFURA}`, '16513351', '16513362');
 
 
 }
 
 
-async function onNewEvent(error, events){
-    if (error) {
-        console.log(error.toString());
-    } else {
-        for (let j = 0; j < events.length; j++) {
-            const e = events[j];
-            if (!e.event) continue;
-            if (e.event != 'Deposit') continue;
-            const u = e.returnValues;
-            console.log(u);
+
+
+async function scanBlockchain(web3, chainName, main, mainBlockStart, lastBlock, maxUsers) {
+    let size = 1000, users = [];
+    let finishProcessing = false;
+    for (let i = mainBlockStart; i < lastBlock; i += size) {
+        if( finishProcessing ){
+            console.log(`1) maxUsers ${maxUsers} reached for chain ${chainName}`);
+            break;
         }
-    }
-
-}
-
-async function scanBlockchain(ctx, start, end, file) {
-    let size = 1000, lines = [];
-    for (let i = start; i < end; i += size) {
         await new Promise(resolve => setTimeout(resolve, 1000));
         const from = i;
         const to = (i + size) - 1;
         try {
-            await ctx.getPastEvents({fromBlock: from, toBlock: to}, onNewEvent);
+            // emit RankClaimed(msg.sender, term, globalRank++, getCurrentAMP(), mintInfo.eaaRate, mintInfo.maturityTs);
+            await main.getPastEvents({fromBlock: from, toBlock: to},
+                async function(error, events){
+                    if (error) {
+                        console.log(error.toString());
+                    } else {
+                        for (let j = 0; j < events.length; j++) {
+                            const e = events[j];
+                            if (!e.event) continue;
+                            if (e.event != 'RankClaimed') continue;
+                            if( users.length >= maxUsers ){
+                                finishProcessing = true;
+                                console.log(`2) maxUsers ${maxUsers} reached for chain ${chainName}`);
+                                return;
+                            }
+                            // event RankClaimed(address indexed user, uint256 term, uint256 rank, uint AMP, uint EAA, uint maturity);
+                            // emit RankClaimed(msg.sender, term, globalRank++, getCurrentAMP(), mintInfo.eaaRate, mintInfo.maturityTs);
+                            const user = e.returnValues.user;
+                            const minter = new web3.eth.Contract(abiMinter, user);
+                            try{
+                                const owner = await minter.owner();
+                                if( owner && users.indexOf(owner) === -1 ){
+                                    users.push(users);
+                                }else if( user && users.indexOf(user) === -1 ){
+                                    users.push(users);
+                                }
+                                if( users.length >= maxUsers ){
+                                    console.log(`3) maxUsers ${maxUsers} reached for chain ${chainName}`);
+                                    finishProcessing = true;
+                                }
+                            }catch(e){
+                                console.log(user, e.toString());
+                            }
+                        }
+                    }
+                }
+            );
         }catch(e){
             console.log(e.toString());
         }
     }
-    // fs.writeFileSync(file, info.join('\n') );
+    fs.writeFileSync(`./data/${chainName}.txt`, users.join('\n') );
 }
 
 main();
