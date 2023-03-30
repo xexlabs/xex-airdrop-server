f144ad07137950de03e61feb76366227f4625d65
regen <120606503+xexcrypto@users.noreply.github.com>
2023-03-30 15:12:07 -0300

    mainnet and testnet data

--- wl-scanner/index.js
+++ wl-scanner/index.js
@@ -1,6 +1,6 @@
 'use strict'
 const fs = require('fs');
-let mainnet = [];
+let mainnet = [], testnet = [];
 
 async function main() {
     /*
@@ -10,24 +10,26 @@ async function main() {
     - AVAX: First 1500 Unique addresses to mint
     - FTM: First 2500 Unique addresses to mint
     * */
-    await processChain(1500, 'bsc', '56');
-    await processChain(1500, 'avax', '42161');
-    await processChain(2500, 'matic', '137');
-    await processChain(2500, 'ftm', '250');
-    await processChain(500, 'eth', '1');
+    await processChain(1500, 'bsc', '56', '97');
+    await processChain(1500, 'avax', '43114', '43113');
+    await processChain(2500, 'matic', '137', '80001');
+    await processChain(2500, 'ftm', '250', '4002');
+    await processChain(500, 'eth', '1', '5');
     fs.writeFileSync('../proof/src/data/mainnet.txt', mainnet.join("\n"));
+    fs.writeFileSync('../proof/src/data/testnet.txt', testnet.join("\n"));
 }
 
-async function processChain(maxUsers, chainName, chainId) {
+async function processChain(maxUsers, chainName, chainIdMainnet, chainIdTestnet) {
     const csv = fs.readFileSync(`./mainnet-data/${chainName}.csv`).toString('utf-8').split("\n");
     for (let i = 1; i < csv.length; i++) {
         const line = csv[i].split(",");
         let address = line[0];
-        if( ! address ) continue;
+        if (!address) continue;
         address = address.replaceAll('"', '');
         if (address.length !== 42) continue;
         if (i >= maxUsers) break;
-        mainnet.push(`${address},${chainId}`);
+        mainnet.push(`${address},${chainIdMainnet}`);
+        testnet.push(`${address},${chainIdTestnet}`);
     }
 }
 
