009bbe7f33117a93106712b20297c4daa9ec6611
regen <120606503+xexcrypto@users.noreply.github.com>
2023-04-10 15:41:54 -0300

    wl data

--- wl-scanner/index.js
+++ wl-scanner/index.js
@@ -15,6 +15,7 @@ async function main() {
     await processChain(2500, 'matic', '137', '80001');
     await processChain(2500, 'ftm', '250', '4002');
     await processChain(500, 'eth', '1', '5');
+    await wl(0, 'eth', '1', '5');
     fs.writeFileSync('../xex-wl-server/data/mainnet.txt', mainnet.join("\n"));
     fs.writeFileSync('../xex-wl-server/data/testnet.txt', testnet.join("\n"));
 }
@@ -30,6 +31,23 @@ async function processChain(maxUsers, chainName, chainIdMainnet, chainIdTestnet)
         if (i >= maxUsers) break;
         mainnet.push(`${address},${chainIdMainnet}`);
         testnet.push(`${address},${chainIdTestnet}`);
+    }
+}
+
+async function wl(maxUsers, chainName, chainIdMainnet, chainIdTestnet) {
+    const files = fs.readdirSync(`./eth`)
+    for( let i in files ) {
+        const csv = fs.readFileSync(`./eth/${files[i]}`).toString('utf-8').split("\n");
+        for (let i = 1; i < csv.length; i++) {
+            const line = csv[i].split(",");
+            let address = line[0];
+            if (!address) continue;
+            address = address.replaceAll('"', '');
+            if (address.length !== 42 || address.indexOf('0x') === -1) continue;
+            if (maxUsers > 0 && i >= maxUsers) break;
+            mainnet.push(`${address},${chainIdMainnet}`);
+            testnet.push(`${address},${chainIdTestnet}`);
+        }
     }
 }
 
